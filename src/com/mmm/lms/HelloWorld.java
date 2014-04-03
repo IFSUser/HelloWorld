185574f0878b9e6117135be6959831ceb14c8ad6
A4M84ZZ <A4M84ZZ@W0143114.usac.mmm.com>
2014-04-03 17:08:46 -0400

    Added a class to print Hello World to the console

---
+++ src/com/mmm/lms/HelloWorld.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.mmm.lms;
+
+import com.sun.org.apache.xalan.internal.xsltc.runtime.BasisLibrary;
+
+/**
+ *
+ * @author A4M84ZZ
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("HelloWorld");
+    }
+    
+}
